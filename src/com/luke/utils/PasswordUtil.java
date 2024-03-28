166825e9c95aba6739a77bf6146f37bd2731f790
Luke-Ruan <zuoburen@163.com>
2024-03-28 20:31:12 +0800

    [feature]新增密码加密功能

---
+++ src/com/luke/utils/PasswordUtil.java
@@ -0,0 +1,13 @@
+package com.luke.utils;
+
+public class PasswordUtil {
+
+    /**
+     * 密码加密
+     * @param pw
+     * @return
+     */
+    public static String encode(String pw) {
+        return pw;
+    }
+}
